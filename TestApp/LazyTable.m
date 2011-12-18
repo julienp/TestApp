a5d96045985165cd05e8113b97f442f9a150e202
julienp <julien@caffeine.lu>
2011-12-18 20:59:22 +0100

    Fix image path and check if image was created successfully

--- TestApp/LazyTable.m
+++ TestApp/LazyTable.m
@@ -14,7 +14,8 @@
     NSCache *cache;
 }
 
-static NSString *imagePaths[] = {@"images/peinture/CD12 002.jpg",  @"images/peinture/CD12 006.jpg",  @"images/peinture/CD12 014.jpg",  @"images/peinture/CD12 022.jpg",  @"images/peinture/CD12 024.jpg",  @"images/peinture/CD12 030.jpg",  @"images/peinture/CD12 031.jpg",  @"images/peinture/CD12 032.jpg",  @"images/peinture/2010_02_9105.jpg",  @"images/peinture/2010_02_9108.jpg",  @"images/peinture/2010_02_9112.jpg",  @"images/peinture/CD2 2010 003.jpg",  @"images/peinture/CD12 026.jpg",  @"images/peinture/CD2 2010 002.jpg",  @"images/peinture/CD12 028.jpg",  @"images/peinture/CD2 2010 006.jpg",  @"images/peinture/CD12 036.jpg",  @"images/peinture/2010_02_9086.jpg",  @"images/peinture/2010_02_9101.jpg",  @"images/peinture/CD12 015.jpg",  @"images/peinture/CD12 017.jpg",  @"images/peinture/CD12 021.jpg",  @"images/peinture/2010_02_9116.jpg",  @"images/peinture/2010_02_9097.jpg",  @"images/peinture/2010_02_9128.jpg",  @"images/peinture/DSCN1478.jpg",  @"images/peinture/DSCN1524.jpg",  @"images/peinture/DSCN1531.jpg",  @"images/peinture/DSCN1607.jpg",  @"images/peinture/DSCN1675.jpg",  @"images/peinture/DSCN1936.jpg",  @"images/peinture/DSCN1940.jpg",  @"images/peinture/DSCN1947.jpg",  @"images/peinture/DSCN1939.jpg"};
+static NSString *imagePaths[] = {@"2010_02_9086.jpg", @"2010_02_9097.jpg", @"2010_02_9101.jpg", @"2010_02_9105.jpg", @"2010_02_9108.jpg", @"2010_02_9112.jpg", @"2010_02_9116.jpg", @"2010_02_9128.jpg", @"CD12 002.jpg", @"CD12 006.jpg", @"CD12 014.jpg", @"CD12 015.jpg", @"CD12 017.jpg", @"CD12 021.jpg", @"CD12 022.jpg", @"CD12 024.jpg", @"CD12 026.jpg", @"CD12 028.jpg", @"CD12 030.jpg", @"CD12 031.jpg", @"CD12 032.jpg", @"CD12 036.jpg", @"CD2 2010 002.jpg", @"CD2 2010 003.jpg", @"CD2 2010 004.jpg", @"CD2 2010 005.jpg", @"CD2 2010 006.jpg", @"DSCN1478.jpg", @"DSCN1524.jpg", @"DSCN1531.jpg", @"DSCN1607.jpg", @"DSCN1675.jpg", @"DSCN1936.jpg", @"DSCN1939.jpg", @"DSCN1940.jpg", @"DSCN1947.jpg", @"DSCN4467.JPG", @"DSCN4482.JPG", @"DSCN4488.JPG", @"DSCN4497.JPG", @"DSCN4518.JPG", @"DSCN4539.JPG", @"DSCN4561.JPG", @"DSCN4566.JPG", @"DSCN4571.JPG", @"DSCN4574.jpg", 
+};
 
 - (id)init
 {
@@ -108,18 +109,20 @@ static NSString *imagePaths[] = {@"images/peinture/CD12 002.jpg",  @"images/pein
         [cell setNeedsLayout];
     } else {
         NSString *urlEncoded = [key stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
-        NSString *path = [@"http://chpoissonnier.com/" stringByAppendingString:urlEncoded];
+        NSString *path = [@"http://chpoissonnier.com/images/peinture/full/" stringByAppendingString:urlEncoded];
         NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:path]];
         [NSURLConnection sendAsynchronousRequest:request queue:queue completionHandler:^(NSURLResponse *response, NSData *data, NSError *error) {
             if (error != nil) {
                 NSLog(@"%@: could not load %@", error, response);
             } else {
                 UIImage *image = [UIImage imageWithData:data];
-                [cache setObject:image forKey:key];
-                dispatch_sync(dispatch_get_main_queue(), ^{
-                    [cell.imageView setImage:image];
-                    [cell setNeedsLayout];
-                });
+                if (image != nil) {
+                    [cache setObject:image forKey:key];
+                    dispatch_sync(dispatch_get_main_queue(), ^{
+                        [cell.imageView setImage:image];
+                        [cell setNeedsLayout];
+                    });
+                }
             }
         }];
     }
